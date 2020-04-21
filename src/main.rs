d552e412e2ecc4be701685c25dae703a8a99240d
Your Name <you@example.com>
2020-04-21 20:23:41 +0200

    added minimal API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+pub use log::{info, trace, warn, error};
+use actix_web::{
+    error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
+
+/// This handler uses json extractor
+async fn index(item: web::Json<MyObj>) -> HttpResponse {
+    info!("model: {:?}", &item);
+    HttpResponse::Ok().json(item.0)
+}
+async fn get_index() -> HttpResponse {
+    let a = MyObj{name : "jej".to_string(), number: 1};
+    info!("model: {:?}", a);
+    HttpResponse::Ok().json(a)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info,actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/")
+                .route(web::post().to(index))
+                .route(web::get().to(get_index)))
+    })
+        .bind(format!("0.0.0.0:{}", std::env::var("PORT").unwrap_or("8080".to_string()).as_str()))?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::dev::Service;
+    use actix_web::{http, test, web, App};
+
+    #[actix_rt::test]
+    async fn test_index() -> Result<(), Error> {
+        let mut app = test::init_service(
+            App::new().service(web::resource("/").route(web::post().to(index))),
+        )
+            .await;
+
+        let req = test::TestRequest::post()
+            .uri("/")
+            .set_json(&MyObj {
+                name: "my-name".to_owned(),
+                number: 43,
+            })
+            .to_request();
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(response_body, r##"{"name":"my-name","number":43}"##);
+
+        Ok(())
+    }
 }
